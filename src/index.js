78f153c74b05b80871fd595e08dc53818cc08d5c
Katya Litvinchuk <77808444+kathleenlii@users.noreply.github.com>
2021-03-15 22:23:24 +0300

    feat: add check implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const objectBrackets = bracketsConfig.reduce(
+        (acc, [firstValue, secondValue]) => ({
+            ...acc,
+            [firstValue]: secondValue,
+        }),
+        {}
+    );
+
+    const stack = [];
+    str.split('').forEach(element => {
+      if(stack[stack.length - 1]) {
+        const closeTag = objectBrackets[stack[stack.length - 1]];
+
+        if(element == closeTag) {
+          stack.pop();
+        } else {
+          stack.push(element)
+        }
+      } else {
+        stack.push(element)
+      }
+    });
+
+    return !stack.length
+};
